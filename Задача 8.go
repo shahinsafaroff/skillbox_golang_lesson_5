9230bc51bcbe49e32266d703063ad8f5a2dde0f0
shahinsafaroff <52258958+shahinsafaroff@users.noreply.github.com>
2020-08-02 13:38:00 +0400

    Update Задача 8.go
    binary search algorithm with finding random number

--- Задача 8.go
+++ Задача 8.go
@@ -1,3 +1,29 @@
 //8. Игра “Угадывание числа” (дополнительно)
+package main
 
-Не Смог рещит эту задачу прощу помоч
+import "fmt"
+
+func main() {
+	fmt.Println("Keep the number from 1 to 10 and I will find your number")
+
+	min := 1
+	max := 10
+	i := 0
+	for i < 11 {
+		number := (min + max) / 2
+		fmt.Println("Founded number:", number, "? type '=' if your number was found, '>' if your number bigger, '<' if your number lesser)")
+
+		var answer string
+		fmt.Scan(&answer)
+		if answer == ">" {
+			min = number + 1
+		} else if answer == "<" {
+			max = number - 1
+		} else if answer == "=" {
+			return
+		} else {
+			fmt.Println("HOOORAAH! Number is found")
+		}
+
+	}
+}
